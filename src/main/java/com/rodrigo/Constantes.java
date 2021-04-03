4c2be386111fe76da49a8d746ea012e3781a4085
devesteves <Esteveshxc@outlook.com>
2021-04-03 18:49:17 -0300

    Mudando paradigma

---
+++ src/main/java/com/rodrigo/Constantes.java
@@ -0,0 +1,8 @@
+package com.rodrigo;
+
+
+
+public interface Constantes  {
+    
+    public static final String URL_CSV = "https://xtr-tinto.consiste.com.br/xtr/xtrAPI.nsf/api.xsp/fileView/md/3E391C29EE1739FD03258609006C7EAA/0/dataset.CSV";
+}
